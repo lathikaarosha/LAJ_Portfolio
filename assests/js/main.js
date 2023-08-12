dc76666542a74ce751171d4640ab1661ccae0958
lathikaarosha <lathikaarosha123@gmail.com>
2023-08-12 20:32:32 +0530

    my portfolio is done

---
+++ assests/js/main.js
@@ -0,0 +1,60 @@
+/* Menu show */
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+
+showMenu('nav-toggle','nav-menu')
+
+/* Active and remove menu */
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction() {
+    // Active link
+    console.log('linkAction function executed');
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+    console.log('Link clicked:', this);
+
+    //remove menu mobile
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction)) 
+
+/* Drop down Container */
+var dropdown = document.querySelectorAll('.dropdown');
+var dropdownArray = Array.prototype.slice.call(dropdown,0);
+dropdownArray.forEach(function(el){
+	var button = el.querySelector('a[data-toggle="dropdown"]'),
+			menu = el.querySelector('.dropdown-menu'),
+			arrow = button.querySelector('i.icon-arrow');
+
+	button.onclick = function(event) {
+		if(!menu.hasClass('show')) {
+			menu.classList.add('show');
+			menu.classList.remove('hide');
+			arrow.classList.add('open');
+			arrow.classList.remove('close');
+			event.preventDefault();
+		}
+		else {
+			menu.classList.remove('show');
+			menu.classList.add('hide');
+			arrow.classList.remove('open');
+			arrow.classList.add('close');
+			event.preventDefault();
+		}
+	};
+})
+
+Element.prototype.hasClass = function(className) {
+    return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className);
+};
